83bfa1c0a4c6120d298e1574fd337f83da3a0640
Jeremy Reiner <jeremy_reiner@redmane.com>
2024-06-17 09:16:33 -0500

    updated bat layout

--- RedManeHelper/mCase/runConsoleApp.bat
+++ RedManeHelper/mCase/runConsoleApp.bat
@@ -1,8 +1,6 @@
 echo Begining Class Sync
 
-cd application location
-
-dotnet run program.cs "connection string" "sql query" "credentials" "target url" "output directory" "output exception directory"
+Direct executable filepath "connection string" "sql query" "credentials" "target url" "output directory" "output exception directory"
 
 echo process completed
 pause
